bb35774a90976d743915a1872c92d6050a3d8c4e
ardeshir <ardeshirojan2@gmail.com>
2024-09-22 05:00:06 +0330

    web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3686474694"
+    serviceWorkerVersion: "2756436925"
   }
 });
